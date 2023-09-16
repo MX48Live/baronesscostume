7cf583035a69e3891f34eff4013273a218b029d2
Watee K <MX48Live@Gmail.com>
2023-09-16 15:13:53 +0700

    Fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { cloudflarePagesAdapter } from '@builder.io/qwik-city/adapters/cloudflare-pages/vite';
 import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
@@ -6,20 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
   return {
-    build: {
-      ssr: true,
-      rollupOptions: {
-        input: ['src/entry.cloudflare-pages.tsx', '@qwik-city-plan'],
-      },
-    },
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths(),
-    cloudflarePagesAdapter({
-      ssg: {
-        include: ['/'],
-        exclude: ['/people/*'],
-      },
-      }),
-    ],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
