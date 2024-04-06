61b98e8b8ccebd568189b865526018421be8c804
MX48Live <mx48live@gmail.com>
2024-04-06 12:03:43 +0700

    Add category and fix open time

--- src/components/web/WorkingHours.tsx
+++ src/components/web/WorkingHours.tsx
@@ -4,11 +4,11 @@ function WorkingHours() {
       <div className="container mx-auto px-4 text-center">
         <h2 className="font-bold text-green text-[30px] mb-7">เวลาทำการ</h2>
         <div className="text-[20px] md:text-[24px] text-green">
-          <div>วันจันทร์ - ศุกร์ 11:00 น. — 19:00 น.</div>
-          <div>
+          <div>วันจันทร์ - วันอาทิตย์ 11:00 น. — 18:00 น.</div>
+          {/* <div>
             วันเสาร์ 11:00 น. — 17:00 น. <br className="md:hidden" />
             (หยุดวันอาทิตย์)
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
